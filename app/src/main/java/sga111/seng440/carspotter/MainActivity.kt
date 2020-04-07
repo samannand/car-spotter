a922af1c7645e0d0723ec0bcc04cc34d04715112
Sam Annand <sga111@uclive.ac.nz>
2020-04-07 17:17:14 +1200

    Added bottom naviation activity default, and linked to landing screen go button

--- app/src/main/java/sga111/seng440/carspotter/MainActivity.kt
+++ app/src/main/java/sga111/seng440/carspotter/MainActivity.kt
@@ -1,12 +1,20 @@
 package sga111.seng440.carspotter
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goButton : Button = findViewById(R.id.goButton)
+        goButton.setOnClickListener {
+            val intent = Intent(this, AppNavigation::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/sga111/seng440/carspotter/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package sga111.seng440.carspotter.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
